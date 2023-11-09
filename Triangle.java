3d5a07dd816f74ea64445d105dbaf7f1e0f7f3b8
AreejAbdulaziz <148686069+AreejAbdulaziz@users.noreply.github.com>
2023-11-09 13:24:39 +0300

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,37 @@
+public class Triangle extends Shape{
+    private double height;
+    private double base;
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public Triangle(double height, double base) {
+        this.height = height;
+        this.base = base;
+    }
+
+    @Override
+    double calculateArea() {
+        return (base*height)/2;
+    }
+
+    @Override
+    double calculateCircumference() {
+        if(base==height)
+        return 3*height;
+        else return 2*height+base;
+    }
+}
